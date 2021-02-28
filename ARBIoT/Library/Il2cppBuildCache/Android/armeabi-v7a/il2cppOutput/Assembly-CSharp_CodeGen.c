c38fe84ddc7d973569ed132a2533eb8cfe06ff6d
DrunkDuckling <soerensen5492@gmail.com>
2021-02-28 22:01:19 +0100

    Initial Commit
    Foundation For Unity

---
+++ ARBIoT/Library/Il2cppBuildCache/Android/armeabi-v7a/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,53 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void PlacementIndicator::Start()
+extern void PlacementIndicator_Start_mC0F1E7B8F84514D2EDEA712785CDC9399A376200 (void);
+// 0x00000002 System.Void PlacementIndicator::Update()
+extern void PlacementIndicator_Update_m04B65B8903A034EA4C717E34BE6B8EF861382FE4 (void);
+// 0x00000003 System.Void PlacementIndicator::.ctor()
+extern void PlacementIndicator__ctor_mC5EF49C57C7F6519CA9E925EC1A0B13C7D3A08BE (void);
+static Il2CppMethodPointer s_methodPointers[3] = 
+{
+	PlacementIndicator_Start_mC0F1E7B8F84514D2EDEA712785CDC9399A376200,
+	PlacementIndicator_Update_m04B65B8903A034EA4C717E34BE6B8EF861382FE4,
+	PlacementIndicator__ctor_mC5EF49C57C7F6519CA9E925EC1A0B13C7D3A08BE,
+};
+static const int32_t s_InvokerIndices[3] = 
+{
+	1814,
+	1814,
+	1814,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	3,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ ARBIoT/Library/Il2cppBuildCache/Android/armeabi-v7a/il2cppOutput/System.Core_CodeGen.c
@@ -0,0 +1,317 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Exception System.Linq.Error::ArgumentNull(System.String)
+extern void Error_ArgumentNull_m0EDA0D46D72CA692518E3E2EB75B48044D8FD41E (void);
+// 0x00000002 System.Exception System.Linq.Error::MoreThanOneMatch()
+extern void Error_MoreThanOneMatch_m4C4756AF34A76EF12F3B2B6D8C78DE547F0FBCF8 (void);
+// 0x00000003 System.Collections.Generic.IEnumerable`1<TSource> System.Linq.Enumerable::Where(System.Collections.Generic.IEnumerable`1<TSource>,System.Func`2<TSource,System.Boolean>)
+// 0x00000004 System.Func`2<TSource,System.Boolean> System.Linq.Enumerable::CombinePredicates(System.Func`2<TSource,System.Boolean>,System.Func`2<TSource,System.Boolean>)
+// 0x00000005 TSource System.Linq.Enumerable::SingleOrDefault(System.Collections.Generic.IEnumerable`1<TSource>,System.Func`2<TSource,System.Boolean>)
+// 0x00000006 System.Boolean System.Linq.Enumerable::Any(System.Collections.Generic.IEnumerable`1<TSource>)
+// 0x00000007 System.Boolean System.Linq.Enumerable::Any(System.Collections.Generic.IEnumerable`1<TSource>,System.Func`2<TSource,System.Boolean>)
+// 0x00000008 System.Void System.Linq.Enumerable/Iterator`1::.ctor()
+// 0x00000009 TSource System.Linq.Enumerable/Iterator`1::get_Current()
+// 0x0000000A System.Linq.Enumerable/Iterator`1<TSource> System.Linq.Enumerable/Iterator`1::Clone()
+// 0x0000000B System.Void System.Linq.Enumerable/Iterator`1::Dispose()
+// 0x0000000C System.Collections.Generic.IEnumerator`1<TSource> System.Linq.Enumerable/Iterator`1::GetEnumerator()
+// 0x0000000D System.Boolean System.Linq.Enumerable/Iterator`1::MoveNext()
+// 0x0000000E System.Collections.Generic.IEnumerable`1<TSource> System.Linq.Enumerable/Iterator`1::Where(System.Func`2<TSource,System.Boolean>)
+// 0x0000000F System.Object System.Linq.Enumerable/Iterator`1::System.Collections.IEnumerator.get_Current()
+// 0x00000010 System.Collections.IEnumerator System.Linq.Enumerable/Iterator`1::System.Collections.IEnumerable.GetEnumerator()
+// 0x00000011 System.Void System.Linq.Enumerable/Iterator`1::System.Collections.IEnumerator.Reset()
+// 0x00000012 System.Void System.Linq.Enumerable/WhereEnumerableIterator`1::.ctor(System.Collections.Generic.IEnumerable`1<TSource>,System.Func`2<TSource,System.Boolean>)
+// 0x00000013 System.Linq.Enumerable/Iterator`1<TSource> System.Linq.Enumerable/WhereEnumerableIterator`1::Clone()
+// 0x00000014 System.Void System.Linq.Enumerable/WhereEnumerableIterator`1::Dispose()
+// 0x00000015 System.Boolean System.Linq.Enumerable/WhereEnumerableIterator`1::MoveNext()
+// 0x00000016 System.Collections.Generic.IEnumerable`1<TSource> System.Linq.Enumerable/WhereEnumerableIterator`1::Where(System.Func`2<TSource,System.Boolean>)
+// 0x00000017 System.Void System.Linq.Enumerable/WhereArrayIterator`1::.ctor(TSource[],System.Func`2<TSource,System.Boolean>)
+// 0x00000018 System.Linq.Enumerable/Iterator`1<TSource> System.Linq.Enumerable/WhereArrayIterator`1::Clone()
+// 0x00000019 System.Boolean System.Linq.Enumerable/WhereArrayIterator`1::MoveNext()
+// 0x0000001A System.Collections.Generic.IEnumerable`1<TSource> System.Linq.Enumerable/WhereArrayIterator`1::Where(System.Func`2<TSource,System.Boolean>)
+// 0x0000001B System.Void System.Linq.Enumerable/WhereListIterator`1::.ctor(System.Collections.Generic.List`1<TSource>,System.Func`2<TSource,System.Boolean>)
+// 0x0000001C System.Linq.Enumerable/Iterator`1<TSource> System.Linq.Enumerable/WhereListIterator`1::Clone()
+// 0x0000001D System.Boolean System.Linq.Enumerable/WhereListIterator`1::MoveNext()
+// 0x0000001E System.Collections.Generic.IEnumerable`1<TSource> System.Linq.Enumerable/WhereListIterator`1::Where(System.Func`2<TSource,System.Boolean>)
+// 0x0000001F System.Void System.Linq.Enumerable/<>c__DisplayClass6_0`1::.ctor()
+// 0x00000020 System.Boolean System.Linq.Enumerable/<>c__DisplayClass6_0`1::<CombinePredicates>b__0(TSource)
+// 0x00000021 System.Void System.Collections.Generic.HashSet`1::.ctor()
+// 0x00000022 System.Void System.Collections.Generic.HashSet`1::.ctor(System.Collections.Generic.IEqualityComparer`1<T>)
+// 0x00000023 System.Void System.Collections.Generic.HashSet`1::.ctor(System.Runtime.Serialization.SerializationInfo,System.Runtime.Serialization.StreamingContext)
+// 0x00000024 System.Void System.Collections.Generic.HashSet`1::System.Collections.Generic.ICollection<T>.Add(T)
+// 0x00000025 System.Void System.Collections.Generic.HashSet`1::Clear()
+// 0x00000026 System.Boolean System.Collections.Generic.HashSet`1::Contains(T)
+// 0x00000027 System.Void System.Collections.Generic.HashSet`1::CopyTo(T[],System.Int32)
+// 0x00000028 System.Boolean System.Collections.Generic.HashSet`1::Remove(T)
+// 0x00000029 System.Int32 System.Collections.Generic.HashSet`1::get_Count()
+// 0x0000002A System.Boolean System.Collections.Generic.HashSet`1::System.Collections.Generic.ICollection<T>.get_IsReadOnly()
+// 0x0000002B System.Collections.Generic.IEnumerator`1<T> System.Collections.Generic.HashSet`1::System.Collections.Generic.IEnumerable<T>.GetEnumerator()
+// 0x0000002C System.Collections.IEnumerator System.Collections.Generic.HashSet`1::System.Collections.IEnumerable.GetEnumerator()
+// 0x0000002D System.Void System.Collections.Generic.HashSet`1::GetObjectData(System.Runtime.Serialization.SerializationInfo,System.Runtime.Serialization.StreamingContext)
+// 0x0000002E System.Void System.Collections.Generic.HashSet`1::OnDeserialization(System.Object)
+// 0x0000002F System.Boolean System.Collections.Generic.HashSet`1::Add(T)
+// 0x00000030 System.Void System.Collections.Generic.HashSet`1::CopyTo(T[])
+// 0x00000031 System.Void System.Collections.Generic.HashSet`1::CopyTo(T[],System.Int32,System.Int32)
+// 0x00000032 System.Void System.Collections.Generic.HashSet`1::Initialize(System.Int32)
+// 0x00000033 System.Void System.Collections.Generic.HashSet`1::IncreaseCapacity()
+// 0x00000034 System.Void System.Collections.Generic.HashSet`1::SetCapacity(System.Int32)
+// 0x00000035 System.Boolean System.Collections.Generic.HashSet`1::AddIfNotPresent(T)
+// 0x00000036 System.Int32 System.Collections.Generic.HashSet`1::InternalGetHashCode(T)
+// 0x00000037 System.Void System.Collections.Generic.HashSet`1/Enumerator::.ctor(System.Collections.Generic.HashSet`1<T>)
+// 0x00000038 System.Void System.Collections.Generic.HashSet`1/Enumerator::Dispose()
+// 0x00000039 System.Boolean System.Collections.Generic.HashSet`1/Enumerator::MoveNext()
+// 0x0000003A T System.Collections.Generic.HashSet`1/Enumerator::get_Current()
+// 0x0000003B System.Object System.Collections.Generic.HashSet`1/Enumerator::System.Collections.IEnumerator.get_Current()
+// 0x0000003C System.Void System.Collections.Generic.HashSet`1/Enumerator::System.Collections.IEnumerator.Reset()
+static Il2CppMethodPointer s_methodPointers[60] = 
+{
+	Error_ArgumentNull_m0EDA0D46D72CA692518E3E2EB75B48044D8FD41E,
+	Error_MoreThanOneMatch_m4C4756AF34A76EF12F3B2B6D8C78DE547F0FBCF8,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
+static const int32_t s_InvokerIndices[60] = 
+{
+	3008,
+	3102,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+	-1,
+};
+static const Il2CppTokenRangePair s_rgctxIndices[12] = 
+{
+	{ 0x02000004, { 22, 4 } },
+	{ 0x02000005, { 26, 9 } },
+	{ 0x02000006, { 35, 7 } },
+	{ 0x02000007, { 42, 10 } },
+	{ 0x02000008, { 52, 1 } },
+	{ 0x02000009, { 53, 21 } },
+	{ 0x0200000B, { 74, 2 } },
+	{ 0x06000003, { 0, 10 } },
+	{ 0x06000004, { 10, 5 } },
+	{ 0x06000005, { 15, 3 } },
+	{ 0x06000006, { 18, 1 } },
+	{ 0x06000007, { 19, 3 } },
+};
+static const Il2CppRGCTXDefinition s_rgctxValues[76] = 
+{
+	{ (Il2CppRGCTXDataType)2, 1181 },
+	{ (Il2CppRGCTXDataType)3, 4412 },
+	{ (Il2CppRGCTXDataType)2, 2047 },
+	{ (Il2CppRGCTXDataType)2, 1744 },
+	{ (Il2CppRGCTXDataType)3, 8167 },
+	{ (Il2CppRGCTXDataType)2, 1232 },
+	{ (Il2CppRGCTXDataType)2, 1748 },
+	{ (Il2CppRGCTXDataType)3, 8180 },
+	{ (Il2CppRGCTXDataType)2, 1746 },
+	{ (Il2CppRGCTXDataType)3, 8173 },
+	{ (Il2CppRGCTXDataType)2, 434 },
+	{ (Il2CppRGCTXDataType)3, 14 },
+	{ (Il2CppRGCTXDataType)3, 15 },
+	{ (Il2CppRGCTXDataType)2, 789 },
+	{ (Il2CppRGCTXDataType)3, 3378 },
+	{ (Il2CppRGCTXDataType)2, 893 },
+	{ (Il2CppRGCTXDataType)2, 930 },
+	{ (Il2CppRGCTXDataType)3, 3379 },
+	{ (Il2CppRGCTXDataType)2, 890 },
+	{ (Il2CppRGCTXDataType)2, 891 },
+	{ (Il2CppRGCTXDataType)2, 929 },
+	{ (Il2CppRGCTXDataType)3, 3377 },
+	{ (Il2CppRGCTXDataType)3, 4413 },
+	{ (Il2CppRGCTXDataType)3, 4415 },
+	{ (Il2CppRGCTXDataType)2, 298 },
+	{ (Il2CppRGCTXDataType)3, 4414 },
+	{ (Il2CppRGCTXDataType)3, 4423 },
+	{ (Il2CppRGCTXDataType)2, 1184 },
+	{ (Il2CppRGCTXDataType)2, 1747 },
+	{ (Il2CppRGCTXDataType)3, 8174 },
+	{ (Il2CppRGCTXDataType)3, 4424 },
+	{ (Il2CppRGCTXDataType)2, 908 },
+	{ (Il2CppRGCTXDataType)2, 944 },
+	{ (Il2CppRGCTXDataType)3, 3383 },
+	{ (Il2CppRGCTXDataType)3, 9555 },
+	{ (Il2CppRGCTXDataType)3, 4416 },
+	{ (Il2CppRGCTXDataType)2, 1183 },
+	{ (Il2CppRGCTXDataType)2, 1745 },
+	{ (Il2CppRGCTXDataType)3, 8168 },
+	{ (Il2CppRGCTXDataType)3, 3382 },
+	{ (Il2CppRGCTXDataType)3, 4417 },
+	{ (Il2CppRGCTXDataType)3, 9554 },
+	{ (Il2CppRGCTXDataType)3, 4430 },
+	{ (Il2CppRGCTXDataType)2, 1185 },
+	{ (Il2CppRGCTXDataType)2, 1749 },
+	{ (Il2CppRGCTXDataType)3, 8181 },
+	{ (Il2CppRGCTXDataType)3, 4662 },
+	{ (Il2CppRGCTXDataType)3, 2462 },
+	{ (Il2CppRGCTXDataType)3, 3384 },
+	{ (Il2CppRGCTXDataType)3, 2461 },
+	{ (Il2CppRGCTXDataType)3, 4431 },
+	{ (Il2CppRGCTXDataType)3, 9556 },
+	{ (Il2CppRGCTXDataType)3, 3381 },
+	{ (Il2CppRGCTXDataType)3, 3039 },
+	{ (Il2CppRGCTXDataType)2, 751 },
+	{ (Il2CppRGCTXDataType)3, 3568 },
+	{ (Il2CppRGCTXDataType)3, 3569 },
+	{ (Il2CppRGCTXDataType)3, 3574 },
+	{ (Il2CppRGCTXDataType)2, 977 },
+	{ (Il2CppRGCTXDataType)3, 3571 },
+	{ (Il2CppRGCTXDataType)3, 9754 },
+	{ (Il2CppRGCTXDataType)2, 727 },
+	{ (Il2CppRGCTXDataType)3, 2455 },
+	{ (Il2CppRGCTXDataType)1, 875 },
+	{ (Il2CppRGCTXDataType)2, 2061 },
+	{ (Il2CppRGCTXDataType)3, 3570 },
+	{ (Il2CppRGCTXDataType)1, 2061 },
+	{ (Il2CppRGCTXDataType)1, 977 },
+	{ (Il2CppRGCTXDataType)2, 2103 },
+	{ (Il2CppRGCTXDataType)2, 2061 },
+	{ (Il2CppRGCTXDataType)3, 3575 },
+	{ (Il2CppRGCTXDataType)3, 3573 },
+	{ (Il2CppRGCTXDataType)3, 3572 },
+	{ (Il2CppRGCTXDataType)2, 208 },
+	{ (Il2CppRGCTXDataType)3, 2463 },
+	{ (Il2CppRGCTXDataType)2, 303 },
+};
+extern const CustomAttributesCacheGenerator g_System_Core_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_System_Core_CodeGenModule;
+const Il2CppCodeGenModule g_System_Core_CodeGenModule = 
+{
+	"System.Core.dll",
+	60,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	12,
+	s_rgctxIndices,
+	76,
+	s_rgctxValues,
+	NULL,
+	g_System_Core_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
