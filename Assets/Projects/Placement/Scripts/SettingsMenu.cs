31ee39010e11b9c95f75dee0dfaed4d660b4ef30
DrunkDuckling <soerensen5492@gmail.com>
2021-03-15 18:03:29 +0100

    redid directory
    Project became to big. Had to add some git ignores. But required that the structure was remade.

---
+++ Assets/Projects/Placement/Scripts/SettingsMenu.cs
@@ -0,0 +1,111 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Setting menu for Hello AR Sample, including settings for different features.
+/// </summary>
+public class SettingsMenu : MonoBehaviour
+{
+
+    [Header("Common Settings")]
+
+    /// <summary>
+    /// Scene object that contains all setting menu UI elements.
+    /// </summary>
+    [SerializeField]
+    private GameObject _menuWindow = null;
+
+    /// <summary>
+    /// Setting menu that contains options for different features.
+    /// </summary>
+    [SerializeField]
+    private GameObject _settingMenuUi = null;
+
+    /// <summary>
+    /// Setting button for opening menu windows.
+    /// </summary>
+    [SerializeField]
+    private Button _settingButton = null;
+
+    [Header("Instant Placement Settings")]
+
+    /// <summary>
+    /// Instant Placement menu for configuring instant placement options.
+    /// </summary>
+    [SerializeField]
+    private GameObject _instantPlacementMenuUi = null;
+
+    /// <summary>
+    /// The button to open instant placement menu.
+    /// </summary>
+    [SerializeField]
+    private Button _instantPlacementButton = null;
+
+
+    /// <summary>
+    /// Unity's Start() method. is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        _menuWindow.SetActive(false);
+        _settingMenuUi.SetActive(false);
+        _settingButton.onClick.AddListener(OnMenuButtonClick);
+
+        _instantPlacementMenuUi.SetActive(false);
+        _instantPlacementButton.onClick.AddListener(OnClickInstantPlacementMenu);
+    }
+
+    /// <summary>
+    /// Unity's OnDestroy() method.
+    /// </summary>
+    public void OnDestroy()
+    {
+        _settingButton.onClick.RemoveListener(OnMenuButtonClick);
+        _instantPlacementButton.onClick.RemoveListener(OnClickInstantPlacementMenu);
+    }
+
+    /// <summary>
+    /// Callback event for closing the setting menu.
+    /// </summary>
+    public void OnMenuClosed()
+    {
+        _menuWindow.SetActive(false);
+        _settingMenuUi.SetActive(false);
+        _instantPlacementMenuUi.SetActive(false);
+        //      _planeDiscoveryGuide.EnablePlaneDiscoveryGuide(true);
+    }
+
+    /// <summary>
+    /// Callback event for option button.
+    /// </summary>
+    private void OnMenuButtonClick()
+    {
+        _menuWindow.SetActive(true);
+//#if ARCORE_FEATURE_INSTANT_PLACEMENT // Both Dpeth and IP are enabled.
+//            _settingMenuUi.SetActive(true);
+//#else // Only Depth is enabled.
+//            _depthMenuUi.SetActive(true);
+//            _depthMenu.OnMenuButtonClicked();
+//#endif
+//            _planeDiscoveryGuide.EnablePlaneDiscoveryGuide(false);
+    }
+
+    /// <summary>
+    /// Callback event for opening instant placement menu.
+    /// </summary>
+    private void OnClickInstantPlacementMenu()
+    {
+        _settingMenuUi.SetActive(false);
+        _instantPlacementMenuUi.SetActive(true);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
