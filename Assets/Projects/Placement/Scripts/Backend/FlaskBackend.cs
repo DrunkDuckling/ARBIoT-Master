b620ef6829a9395efe248c60bd28a28951648e08
DrunkDuckling <soerensen5492@gmail.com>
2021-04-14 16:42:26 +0200

    playing with backend conn
    Not working atm.

---
+++ Assets/Projects/Placement/Scripts/Backend/FlaskBackend.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace arbiot
+{
+    public class FlaskBackend : MonoBehaviour, IFB
+    {
+        [SerializeField] private string _uri = "http://localhost:5000/";
+
+        public void GetAddressesPointData(Action<string> callback)
+        {
+            Debug.Log("Get room point data");
+            StartCoroutine(GetRequest(_uri + "rum/p", callback));
+        }
+
+        public void GetAddressesAreaData(Action<string> callback)
+        {
+            Debug.Log("Get room area data");
+            StartCoroutine(GetRequest(_uri + "rum/a", callback));
+        }
+
+        public void GetRoomData(string roomId, Action<string> callback)
+        {
+            StartCoroutine(GetRequest(_uri + "getData/" + roomId, callback));
+        }
+
+        [Obsolete]
+        IEnumerator GetRequest(string uri, Action<string> callback)
+        {
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+            {
+                // Request and wait for the desired page.
+                yield return webRequest.SendWebRequest();
+
+                string[] pages = uri.Split('/');
+                int page = pages.Length - 1;
+
+                if (webRequest.isNetworkError || webRequest.isHttpError)
+                {
+                    Debug.Log(pages[page] + ": Error: " + webRequest.error);
+                    Debug.Log(pages[page] + ": Error: " + webRequest.downloadHandler.text);
+                }
+                else
+                {
+                    string data = webRequest.downloadHandler.text;
+                    if (data[0].ToString() == "[")
+                    {
+                        data = data.Substring(1, data.Length - 3);
+                    }
+
+                    Debug.Log(data);
+                    callback(data);
+                }
+            }
+        }
+
+
+        private void OnDestroy()
+        {
+            StopAllCoroutines();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/Projects/Placement/Scripts/Backend/IFB.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace arbiot
+{
+    public interface IFB
+    {
+        void GetAddressesPointData(Action<string> callback);
+        void GetAddressesAreaData(Action<string> callback);
+        void GetRoomData(string roomid, Action<string> callback);
+    }
+}
